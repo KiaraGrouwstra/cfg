f31eced1915a64145a3e7b2bd3fbe72bbb76d6eb
Kiara Grouwstra <kiara@bij1.org>
2023-04-08 18:26:13 +0200

    clean comments

--- hosts/steen/configuration.nix
+++ hosts/steen/configuration.nix
@@ -49,10 +49,10 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # X11
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # GNOME
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
@@ -62,10 +62,10 @@
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
+  # printing
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # sound
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -74,18 +74,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kiara = {
     isNormalUser = true;
     description = "Kiara Grouwstra";
