58897c9217d023d0c69b364c53a4ba69b495a704
Kiara Grouwstra <kiara@bij1.org>
2023-04-08 18:35:48 +0200

    + mtr

--- home-manager/kiara/home.nix
+++ home-manager/kiara/home.nix
@@ -8,6 +8,7 @@
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
+    mtr
     vim
     wget
     curl
--- hosts/steen/configuration.nix
+++ hosts/steen/configuration.nix
@@ -117,13 +117,8 @@
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs = {
+    mtr.enable = true;
 
   # List services that you want to enable:
 
