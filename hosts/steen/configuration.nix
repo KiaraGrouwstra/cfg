bd1f71bd4869aaf71bec319585ab7378aded7499
Kiara Grouwstra <kiara@bij1.org>
2023-04-08 18:27:39 +0200

    add system packages

--- hosts/steen/configuration.nix
+++ hosts/steen/configuration.nix
@@ -104,8 +104,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim
+    wget
+    curl
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
