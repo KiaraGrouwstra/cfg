8ee31ba8395f5a68c8ffd8c1cc90b1ea358d5e62
Kiara Grouwstra <kiara@bij1.org>
2023-04-08 18:27:55 +0200

    add user packages

--- home-manager/kiara/home.nix
+++ home-manager/kiara/home.nix
@@ -6,6 +6,41 @@
   home.username = "kiara";
   home.homeDirectory = "/home/kiara";
 
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+    gnupg
+    pinentry-gtk2
+    zsh
+    emacs
+    amp
+    wezterm
+    firefox
+    thunderbird
+    signal-desktop-beta
+    gnome.gnome-tweaks
+    vscodium
+    libreoffice-fresh
+    thefuck
+    pass
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    zsh-history-search-multi-word
+    paper-icon-theme
+    adementary-theme
+    bibata-cursors
+    emacsPackages.guix
+    keepassxc
+    podman-compose
+    texlive.combined.scheme-minimal
+    tealdeer
+    browserpass
+    most
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
