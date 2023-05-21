8479a38765555b73ab8d3d8f7b3de786fd47c83b
Kiara Grouwstra <kiara@bij1.org>
2023-05-21 08:08:33 +0200

    docker

--- hosts/steen/configuration.nix
+++ hosts/steen/configuration.nix
@@ -162,10 +162,14 @@
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      # dockerCompat = true;
+      # NOTE: this doesn't replace Docker Swarm
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.dnsname.enable = true;
+    };
+    docker = {
+      enable = true;
     };
   };
 
