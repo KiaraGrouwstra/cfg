f47a86e12e622f1b8851a94e8a51b21c39740bbe
Kiara Grouwstra <kiara@bij1.org>
2023-07-15 18:48:04 +0200

    postgresql

--- hosts/steen/configuration.nix
+++ hosts/steen/configuration.nix
@@ -223,6 +223,15 @@
     wantedBy = [ "multi-user.target" ];
   };
 
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
+  };
+
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
