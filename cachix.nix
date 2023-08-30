17a5e13b58c854fb3dd74b57679ccd6de96df079
Kiara Grouwstra <kiara@bij1.org>
2023-08-30 16:59:01 +0200

    add community cache https://nixos.wiki/wiki/Binary_Cache#Using_a_binary_cache

--- cachix.nix
+++ cachix.nix
@@ -9,5 +9,14 @@ let
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
 in {
   inherit imports;
-  nix.settings.substituters = ["https://cache.nixos.org/"];
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+  # .substituters = ["https://cache.nixos.org/"];
 }
