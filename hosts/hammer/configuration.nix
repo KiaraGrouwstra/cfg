0bd310f3a0d2d97ffff817cc631d40bcb610ed47
Kiara Grouwstra <kiara@bij1.org>
2023-10-04 22:12:13 +0200

    update device

--- hosts/hammer/configuration.nix
+++ hosts/hammer/configuration.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -42,23 +43,35 @@
     LC_TIME = "nl_NL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # X11
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # GNOME
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.defaultSession = "gnome";
 
-  # Configure keymap in X11
+  services.xserver.desktopManager.gnome = {
+    extraGSettingsOverrides = ''
+      [org.gnome.shell]
+      favorite-apps=['org.gnome.Nautilus.desktop', 'org.wezfurlong.wezterm.desktop', 'firefox.desktop', 'thunderbird.desktop', 'signal-desktop-beta.desktop', 'codium.desktop', 'org.keepassxc.KeePassXC.desktop']
+    '';
+
+    extraGSettingsOverridePackages = with pkgs; [
+      gnome.gnome-shell
+    ];
+  };
+
+  # keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
+  # printing
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # sound
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -67,67 +80,158 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  hardware.bluetooth.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kiara = {
     isNormalUser = true;
-    description = "Kiara Grouwstra";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
+    description = "輝愛来 (kiara)";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "adbusers"
+      "docker"
+      "libvirtd"
     ];
+    shell = pkgs.zsh;
   };
 
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kiara";
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  # guix
+  users.extraUsers = lib.fold (a: b: a // b) { } (builtins.map
+    (i: {
+      "guixbuilder${i}" = {
+        group = "guixbuild";
+        extraGroups = [ "guixbuild" ];
+        home = "/var/empty";
+        shell = pkgs.shadow;
+        description = "Guix build user ${i}";
+        isSystemUser = true;
+      };
+    }) [ "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" ]);
+  users.extraGroups.guixbuild = { name = "guixbuild"; };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese
+    gnome-music
+    epiphany
+    geary
+    totem
+    tali
+    iagno
+    hitori
+    atomix
+  ]);
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim
+    wget
     home-manager
+    curl
+    git
+    cachix
+    zoom-us
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs = {
+    mtr.enable = true;
+    zsh.enable = true;
+    browserpass.enable = true;
+    firefox.nativeMessagingHosts.browserpass = true;
+  };
+
+  environment.shells = with pkgs; [ zsh ];
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
+  # needed for gnupg
+  services.pcscd.enable = true;
+  services.dbus.packages = [ pkgs.gcr ];
+  # if not working run: `pkill gpg-agent`
+
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall = { 
+    enable = true;
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+  };
+
+  fonts.fonts = with pkgs; [
+    powerline-fonts
+    twemoji-color-font
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    noto-fonts-emoji-blob-bin
+  ];
+
+  # /root/.config/guix/current/lib/systemd/system/guix-daemon.service
+  systemd.services.guix-daemon = {
+    enable = true;
+    description = "Build daemon for GNU Guix";
+    serviceConfig = {
+      ExecStart = "/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild";
+      Environment = [ "GUIX_LOCPATH=/var/guix/profiles/per-user/root/guix-profile/lib/locale" "LC_ALL=en_US.utf8" ];
+      RemainAfterExit = "yes";
+
+      # See <https://lists.gnu.org/archive/html/guix-devel/2016-04/msg00608.html>.
+      # Some package builds (for example, go@1.8.1) may require even more than
+      # 1024 tasks.
+      TasksMax = "8192";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
+  };
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  services.flatpak.enable = true;
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-run"
+    "zoom"
+    "zoom-us"
+  ];
+
+  # https://github.com/NixOS/nixpkgs/pull/210453#issuecomment-1410035331
+  nixpkgs.config.firefox.speechSynthesisSupport = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
