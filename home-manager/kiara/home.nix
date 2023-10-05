24cb493631a5e405104d151178fc29a4c6daa563
Kiara Grouwstra <kiara@bij1.org>
2023-10-05 19:45:38 +0200

    git config

--- home-manager/kiara/home.nix
+++ home-manager/kiara/home.nix
@@ -325,10 +325,60 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.file = {
-    ".gitignore" = {
-      source = ./dotfiles/.gitignore;
+  programs.git = {
+    enable = true;
+    userName  = "Kiara Grouwstra";
+    userEmail = "kiara@bij1.org";
+    delta.enable = true;
+    ignores = [
+      # Compiled source #
+      ###################
+      "*.com"
+      "*.class"
+      "*.dll"
+      "*.exe"
+      "*.o"
+      "*.so"
+
+      # Temporary files #
+      ###################
+      "*.swp"
+      "*.swo"
+      "*~"
+
+      # Packages #
+      ############
+      "*.7z"
+      "*.dmg"
+      "*.gz"
+      "*.iso"
+      "*.jar"
+      "*.rar"
+      "*.tar"
+      "*.zip"
+
+      # Logs #
+      ######################
+      "*.log"
+
+      # OS generated files #
+      ######################
+      ".DS_Store*"
+      "ehthumbs.db"
+      "Icon?"
+      "Thumbs.db"
+
+      # Editor files #
+      ################
+      ".vscode"
+    ];
+    extraConfig = {
+      push.autoSetupRemote = true;
+      branch.autoSetupMerge = "simple";
     };
+  };
+
+  home.file = {
   };
 
 }
