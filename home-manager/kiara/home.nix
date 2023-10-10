4b9739173dcafb725d5bf711e571902aea8a994d
Kiara Grouwstra <kiara@bij1.org>
2023-10-10 03:17:21 +0200

    mime: office

--- home-manager/kiara/home.nix
+++ home-manager/kiara/home.nix
@@ -192,35 +192,59 @@
     "inode/directory" = [ "org.gnome.Nautilus.desktop" "lapce.desktop" "codium.desktop" ];
     "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
     "text/calendar" = [ "org.gnome.Calendar.desktop" ];
-    "application/pdf" = ["org.gnome.Evince.desktop"];
+    "application/pdf" = [ "org.gnome.Evince.desktop" ];
     "application/x-code-workspace" = [ "lapce.desktop" "codium.desktop" ];
     "text/plain" = [ "lapce.desktop" "codium.desktop" ];
-    "text/markdown" = ["codium.desktop"];
-    "image/jpeg"                = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/bmp"                 = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/gif"                 = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/jpg"                 = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/pjpeg"               = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/png"                 = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/tiff"                = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/webp"                = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-bmp"               = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-gray"              = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-icb"               = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-ico"               = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-png"               = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-portable-anymap"   = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-portable-bitmap"   = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-portable-graymap"  = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-portable-pixmap"   = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-xbitmap"           = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-xpixmap"           = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-pcx"               = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/svg+xml"             = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/svg+xml-compressed"  = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/vnd.wap.wbmp"        = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-    "image/x-icns"              = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-  };
+    "text/markdown" = [ "codium.desktop" ];
+  } // (builtins.listToAttrs (map (type: { "image/${type}" = [ "org.gnome.eog.desktop" "gimp.desktop" ]; }) [
+    "jpeg"
+    "bmp"
+    "gif"
+    "jpg"
+    "pjpeg"
+    "png"
+    "tiff"
+    "webp"
+    "x-bmp"
+    "x-gray"
+    "x-icb"
+    "x-ico"
+    "x-png"
+    "x-portable-anymap"
+    "x-portable-bitmap"
+    "x-portable-graymap"
+    "x-portable-pixmap"
+    "x-xbitmap"
+    "x-xpixmap"
+    "x-pcx"
+    "svg+xml"
+    "svg+xml-compressed"
+    "vnd.wap.wbmp"
+    "x-icns"
+  ])) // (builtins.listToAttrs (map (type: { "application/${type}" = [ "writer.desktop" ]; }) [
+    "vnd.openxmlformats-officedocument.wordprocessingml.document"
+    "vnd.openxmlformats-officedocument.wordprocessingml.template"
+    "msword"
+    "vnd.ms-word.document.macroEnabled.12"
+    "vnd.ms-word.template.macroEnabled.12"
+  ])) // (builtins.listToAttrs (map (type: { "application/vnd.${type}" = [ "calc.desktop" ]; }) [
+    "openxmlformats-officedocument.spreadsheetml.sheet"
+    "openxmlformats-officedocument.spreadsheetml.template"
+    "ms-excel"
+    "ms-excel.sheet.macroEnabled.12"
+    "ms-excel.template.macroEnabled.12"
+    "ms-excel.addin.macroEnabled.12"
+    "ms-excel.sheet.binary.macroEnabled.12"
+  ])) // (builtins.listToAttrs (map (type: { "application/vnd.${type}" = [ "impress.desktop" ]; }) [
+    "openxmlformats-officedocument.presentationml.presentation"
+    "openxmlformats-officedocument.presentationml.template"
+    "openxmlformats-officedocument.presentationml.slideshow"
+    "ms-powerpoint"
+    "ms-powerpoint.addin.macroEnabled.12"
+    "ms-powerpoint.presentation.macroEnabled.12"
+    "ms-powerpoint.template.macroEnabled.12"
+    "ms-powerpoint.slideshow.macroEnabled.12"
+  ]));
 
   programs.zsh = {
     enable = true;
