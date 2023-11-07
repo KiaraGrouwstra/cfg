c5e9adf0c0d07ee82c75ffa31d60e79f0c1b958c
Kiara Grouwstra <kiara@bij1.org>
2023-11-07 23:56:14 +0100

    wip: terminal mimes

--- home-manager/kiara/desktop.nix
+++ home-manager/kiara/desktop.nix
@@ -1,38 +1,46 @@
 { pkgs, ... }:
 
+let
+  uncompress = command: ''echo %f \| while read -r file \; do echo "\$\(dirname \$file\)/\$\(basename \$file .tar.gz\)" \| while read -r base \; do mkdir -p \$base \&\& ${command} \&\& xdg-open \$base \& \; done \; done'';
+  commandDesktop = (name: command: mimeTypes:
+    (pkgs.makeDesktopItem {
+      name = name;
+      desktopName = name;
+      genericName = name;
+      terminal = true;
+      exec = command;
+      # exec = "${pkgs.kitty}/bin/kitty ${command}";
+      icon = "utilities-terminal";
+      categories = [ "Office" "Viewer" ]; # https://askubuntu.com/a/674411/332744
+      mimeTypes = mimeTypes;
+    })
+  );
+in
+
 {
 
   home.packages = with pkgs; [
 
-    (makeDesktopItem {
-      name = "less";
-      desktopName = "Less";
-      genericName = "Less";
-      terminal = true;
-      exec = "${wezterm}/bin/wezterm ${less}/bin/less";
-      icon = "utilities-terminal";
-      categories = [ "Office" "Viewer" ]; # https://askubuntu.com/a/674411/332744
-      mimeTypes = [
-        "text/plain"
-        "text/html"
-        "text/markdown" # mdcat / pandoc
-        "application/json" # jq
-        "application/vnd.smart.notebook" # pandoc
-      ];
-    })
+    (commandDesktop "less" "${less}/bin/less" [
+      "text/plain"
+      "text/html"
+      "text/markdown" # mdcat / pandoc
+      "application/json" # jq
+      "application/vnd.smart.notebook" # pandoc
+    ])
 
-    (makeDesktopItem {
-      name = "glow";
-      desktopName = "glow";
-      genericName = "glow";
-      terminal = true;
-      exec = "${wezterm}/bin/wezterm ${glow}/bin/glow";
-      icon = "utilities-terminal";
-      categories = [ "Office" "Viewer" ];
-      mimeTypes = [
-        "text/markdown"
-      ];
-    })
+    (commandDesktop "glow" "${glow}/bin/glow" [
+      "text/markdown"
+    ])
+
+    (commandDesktop "tar" (uncompress ''${gnutar}/bin/tar --directory=\$base -xvf \$file'') [
+      "application/x-gzip"
+      "application/x-tar"
+    ])
+
+    (commandDesktop "zip" (uncompress ''${unzip}/bin/unzip \$file -d \$base'') [
+      "application/x-zip"
+    ])
 
   ];
 
