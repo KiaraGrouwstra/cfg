ed9c9e27a9d0e48493116567ba36d94fc1f27841
Kiara Grouwstra <kiara@bij1.org>
2023-11-07 20:52:27 +0100

    tts: services.gnome.at-spi2-core (orca)

--- hosts/hammer/configuration.nix
+++ hosts/hammer/configuration.nix
@@ -206,6 +206,8 @@
     };
   };
 
+  services.gnome.at-spi2-core.enable = true; # orca
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
