8fb2e20600c38096a5fb1b8c7387a9bbd60f7c14
Kiara Grouwstra <kiara@bij1.org>
2023-11-08 07:09:48 +0100

    hyprland highlighting: catppuccin

--- home-manager/kiara/hyprland.nix
+++ home-manager/kiara/hyprland.nix
@@ -64,7 +64,7 @@
           gaps_in = 5;
           gaps_out = 20;
           border_size = 2;
-          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          "col.active_border" = "rgba(ef9f76ee) rgba(eebebeee) 45deg";
           "col.inactive_border" = "rgba(595959aa)";
 
           layout = "dwindle";
