63abd350c23f8ef309fbaeec39eedf7644b258bd
Kiara Grouwstra <kiara@bij1.org>
2023-11-15 02:59:34 +0100

    faster wofi

--- home-manager/kiara/wofi.nix
+++ home-manager/kiara/wofi.nix
@@ -52,7 +52,7 @@
           border: 3px solid @accent;
           border-radius: 7px;
           background-color: @bg;
-          animation: slideIn 0.5s ease-in-out both;
+          animation: slideIn 0.1s ease-in-out both;
       }
 
       /* Slide In */
@@ -72,7 +72,7 @@
           padding: 10px;
           border: none;
           background-color: @bg;
-          animation: fadeIn 0.5s ease-in-out both;
+          animation: fadeIn 0.1s ease-in-out both;
       }
 
       /* Fade In */
@@ -108,7 +108,7 @@
           border: none;
           color: @accent;
           background-color: @bg2;
-          animation: fadeIn 0.5s ease-in-out both;
+          animation: fadeIn 0.1s ease-in-out both;
       }
 
       /* Text */
@@ -117,7 +117,7 @@
           padding: 10px;
           border: none;
           color: @txt;
-          animation: fadeIn 0.5s ease-in-out both;
+          animation: fadeIn 0.1s ease-in-out both;
       }
 
       /* Selected Entry */
