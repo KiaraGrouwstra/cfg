90272fea7342439f9e6135ed9a02f5480d1759c9
Kiara Grouwstra <kiara@bij1.org>
2023-11-24 02:15:19 +0100

    syncthing

--- home-manager/kiara/home.nix
+++ home-manager/kiara/home.nix
@@ -321,6 +321,8 @@
     enableZshIntegration = true;
   };
 
+  services.syncthing.enable = true;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
