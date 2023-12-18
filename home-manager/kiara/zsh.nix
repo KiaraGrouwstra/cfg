74263c07151067122a52904d00c003d52cec7927
Kiara Grouwstra <kiara@bij1.org>
2023-12-18 16:36:40 +0100

    Revert "zsh: explicit commands"
    This reverts commit 727b036bd85c99a3c71e9c7e84788a199a2989dc.

--- home-manager/kiara/zsh.nix
+++ home-manager/kiara/zsh.nix
@@ -1,6 +1,5 @@
-{ pkgs, inputs, lib, ... }:
+{ pkgs, lib, ... }:
 
-with (import ./commands.nix { pkgs = pkgs; inputs = inputs; });
 {
   programs.zsh = {
     enable = true;
@@ -19,25 +18,25 @@ with (import ./commands.nix { pkgs = pkgs; inputs = inputs; });
       enable = true;
       # https://github.com/ohmyzsh/ohmyzsh/wiki/Plugins
       plugins = [
-        "${git}"
-        "${thefuck}"
+        "git"
+        "thefuck"
       ];
       theme = "agnoster";
     };
     sessionVariables = {
       GTK_THEME = "Catppuccin-Frappe-Compact-Maroon-Dark";
-      EDITOR = "nvim"; # programs.firefox.package
-      VISUAL = "${less}";
-      BROWSER = "firefox"; # programs.firefox.package
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      BROWSER = "firefox";
     };
     shellAliases = {
       docker-compose = "podman-compose";
-      cat = "${bat}";
-      ls = "${eza}";
+      cat = "bat";
+      ls = "eza";
       vi = "nvim";
       vim = "nvim";
-      ssh = "${kitten} ${ssh}";
-      grep = "${rg}";
+      ssh = "kitten ssh";
+      grep = "rg";
     };
     initExtra = (lib.readFile ./zsh.zsh);
   };
