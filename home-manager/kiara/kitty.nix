d1d64f3e36d85c44b03b23254d37718f7da1a09f
Kiara Grouwstra <kiara@bij1.org>
2023-12-18 15:10:06 +0100

    kitty env vars

--- home-manager/kiara/kitty.nix
+++ home-manager/kiara/kitty.nix
@@ -25,6 +25,8 @@ in
       close_on_child_death = true; # yes
       confirm_os_window_close = 0;
       shell_integration = "enabled";
+      terminal = ".";
+      editor = ".";
       scrollback_lines = 4000;
       scrollback_pager_history_size = 2048;
       window_padding_width = 15;
