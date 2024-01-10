b43c6f0458ccc8ea9a0184ebe03c2dd1935c7e74
Kiara Grouwstra <kiara@bij1.org>
2024-01-11 00:13:14 +0100

    weechat plugins

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,8 +8,27 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+
+    weechat = prev.weechat.override {
+      configure = { availablePlugins, ... }: {
+        scripts = with prev.weechatScripts; [
+          weechat-otr
+          wee-slack
+          edit
+          highmon
+          url_hint
+          multiline
+          weechat-go
+          zncplayback
+          weechat-grep
+          colorize_nicks
+          buffer_autoset
+          weechat-matrix
+          weechat-autosort
+          weechat-notify-send
+        ];
+      };
+    };
+
   };
 }
