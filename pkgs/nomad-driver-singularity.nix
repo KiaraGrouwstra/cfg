5816698ed4619e2666e78b5c7a634459f8299b1a
Kiara Grouwstra <kiara@bij1.org>
2024-01-24 17:40:03 +0100

    add nomad driver package files

---
+++ pkgs/nomad-driver-singularity.nix
@@ -0,0 +1,22 @@
+{ lib, inputs, buildGoModule }:
+
+buildGoModule rec {
+  pname = "nomad-driver-singularity";
+  version = "1.0.0-alpha2"; # v
+
+  src = inputs.nomad-driver-singularity;
+
+  vendorHash = "";
+
+  subPackages = [ "." ];
+
+  # doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://github.com/hpcng/nomad-driver-singularity";
+    description = "HashiCorp Nomad driver plugin - Singularity";
+    platforms = platforms.linux;
+    license = licenses.mpl2;
+    maintainers = with maintainers; [ KiaraGrouwstra ];
+  };
+}
