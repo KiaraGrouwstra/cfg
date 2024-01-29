f3c8b8df8a899044044a74db29366d82c058e48f
Kiara Grouwstra <kiara@bij1.org>
2024-01-29 03:38:17 +0100

    firefox policies

--- hosts/hammer/firefox.nix
+++ hosts/hammer/firefox.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
 
@@ -10,19 +10,70 @@
     wrapperConfig = {};
     preferencesStatus = "clear";
     # about:config
-    preferences = {};
+    preferences = {
+      # disable sound when searching through a page
+      "accessibility.typeaheadfind.enablesound" = false;
+      # enable dark pdfjs theme
+      "pdfjs.viewerCssTheme" = 2;
+      # break restrictions on extensions
+      "privacy.resistFingerprinting.block_mozAddonManager" = true;
+      # "extensions.webextensions.restrictedDomains" = ""; # use only trusted extensions
+      # enable userChrome.css
+      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    };
     # https://mozilla.github.io/policy-templates/
     policies = {
-      DisableFirefoxAccounts = false;
-      DontCheckDefaultBrowser = true;
+      AppAutoUpdate = false;
       CaptivePortal = false;
+      Cookies = {
+        AcceptThirdParty = "from-visited";
+        ExpireAtSessionEnd = false; # true
+        Locked = true;
+        RejectTracker = true;
+        Behavior = "reject-tracker";
+        BehaviorPrivateBrowsing = "reject-tracker-and-partition-foreign";
+      };
+      DisableAppUpdate = true;
+      DisableFirefoxAccounts = false;
       DisableFirefoxStudies = true;
+      DisableForgetButton = true;
+      DisableFormHistory = true; # false
+      DisableMasterPasswordCreation = true;
+      DisablePasswordReveal = true;
       DisablePocket = true;
+      DisableProfileImport = true;
+      DisableProfileRefresh = true;
+      DisableSafeMode = false;
+      DisableSecurityBypass = true;
+      DisableSetDesktopBackground = true;
+      DisableSystemAddonUpdate = true;
       DisableTelemetry = true;
+      DisplayBookmarksToolbar = false;
+      DontCheckDefaultBrowser = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = false;
+        # Exceptions = ["https://example.com"];
+      };
+      ExtensionUpdate = false;
       FirefoxHome = {
         Pocket = false;
         Snippets = false;
       };
+      FirefoxSuggest = {
+        WebSuggestions = false;
+        SponsoredSuggestions = false;
+        ImproveSuggest = false;
+        Locked = true;
+      };
+      InstallAddonsPermission.Default = false;
+      ManualAppUpdateOnly = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      OfferToSaveLoginsDefault = false;
+      PasswordManagerEnabled = true;
       UserMessaging = {
         ExtensionRecommendations = false;
         SkipOnboarding = true;
@@ -33,7 +84,9 @@
       };
       SearchEngines = {
         Default = "duckduckgo";
+        PreventInstalls = true;
       };
+      SearchSuggestEnabled = false;
     };
     languagePacks = [
       "en-US"
