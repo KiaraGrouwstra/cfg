7280a3663ae8d37784beaa7a1db9345c7c2d1bba
Kiara Grouwstra <kiara@bij1.org>
2024-02-22 21:21:09 +0100

    ditch manual d-bus

--- hosts/hammer/niri.nix
+++ hosts/hammer/niri.nix
@@ -5,9 +5,6 @@
 {
   services.xserver.displayManager.defaultSession = "niri";
 
-  # xdg-desktop-portal uses D-Bus
-  services.dbus.enable = true;
-
   # qt wayland: https://discourse.nixos.org/t/problem-with-qt-apps-styling/29450/8
   qt.platformTheme = "qt5ct";
 
@@ -15,5 +12,4 @@
 
   environment.systemPackages = with pkgs; [swaybg];
 
-  # programs.xwayland.enable = true;
 }
