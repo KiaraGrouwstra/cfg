b49aa4a3a9aeeb89a2c37fa3a7f5b44b4e9e1e5c
Kiara Grouwstra <kiara@bij1.org>
2024-02-24 02:19:24 +0100

    thefuck: nix run

--- overlays/thefuck.nix
+++ overlays/thefuck.nix
@@ -3,10 +3,10 @@
 # https://github.com/nvbn/thefuck/pull/1393
 pkgs.thefuck.overridePythonAttrs (old: {
   src = pkgs.fetchFromGitHub {
-    owner = "thenbe";
+    owner = "KiaraGrouwstra";
     repo = "thefuck";
-    rev = "6202d325100c51076692d998e36a293955101365";
-    hash = "sha256-ZTBOK6sVHQYPmvTSLQcdnyyL+89cFGjVm8czzBC2Ecg=";
+    rev = "74f3244543a4619c09b40f6680245834c0204dd0";
+    hash = "sha256-jxfJhP4HhHVBBA725clSS79nrHf7s2Aq5nnKltR8OX8=";
   };
   doCheck = false;
 })
