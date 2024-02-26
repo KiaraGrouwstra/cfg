c515af8a063d807d2f67190cfc2b0ce4281d7c26
Kiara Grouwstra <kiara@bij1.org>
2024-02-26 03:01:56 +0100

    add thefuck rules to fix nix hash mismatches

---
+++ home-manager/kiara/dotfiles/.config/thefuck/rules/nix_hash.py
@@ -0,0 +1,22 @@
+from thefuck.specific.nix import nix_available
+import re
+
+enabled_by_default = nix_available
+
+pattern = re.compile('specified: (sha\d+:[^=]+=)\n\s*got: (sha\d+:[^=]+=)$')
+
+def match(command):
+    return (
+        "hash mismatch importing path" in command.output
+    )
+
+def get_new_command(command):
+    result = pattern.search(command.output)
+    if result:
+        old_hash = result.group(1)
+        new_hash = result.group(2)
+        return [
+            "sed -i -e s%{}%{}% {flake.lock,**/*.nix}; {}".format(old_hash, new_hash, command.script)
+        ]
+    else:
+        return []
---
+++ home-manager/kiara/dotfiles/.config/thefuck/rules/nix_nar.py
@@ -0,0 +1,22 @@
+from thefuck.specific.nix import nix_available
+import re
+
+enabled_by_default = nix_available
+
+pattern = re.compile("expected '(sha\d+-[^=]+=)', got '(sha\d+-[^=]+=)'$")
+
+def match(command):
+    return (
+        "NAR hash mismatch in input" in command.output
+    )
+
+def get_new_command(command):
+    result = pattern.search(command.output)
+    if result:
+        old_hash = result.group(1)
+        new_hash = result.group(2)
+        return [
+            "sed -i -e s%{}%{}% {flake.lock,**/*.nix}; {}".format(old_hash, new_hash, command.script)
+        ]
+    else:
+        return []
