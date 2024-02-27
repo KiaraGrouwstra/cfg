504b14c9914cdacdb680fcb4ba3b00eb737ca9c8
Kiara Grouwstra <kiara@bij1.org>
2024-02-27 12:46:34 +0100

    auto-enhancd

--- home-manager/kiara/zsh.zsh
+++ home-manager/kiara/zsh.zsh
@@ -8,12 +8,12 @@ GUIX_PROFILE="$HOME/.guix-profile"
 # flatpak
 export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
 
-# kubectl
-[[ $commands[kubectl] ]] && source <(kubectl completion zsh)
-
 # markdown
 function mrkd() { pandoc ${@:1:2} | lynx -stdin; }
+# TODO: style browser for local html files
 function markdown() {
   pandoc $1 > /tmp/$1.html
   xdg-open /tmp/$1.html
 }
+
+cd
