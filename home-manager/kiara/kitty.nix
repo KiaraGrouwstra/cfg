1720ef38bd219dd452377ddaa65307727652fff4
Kiara Grouwstra <kiara@bij1.org>
2024-03-11 00:21:25 +0100

    ditch kitty xterm override

--- home-manager/kiara/kitty.nix
+++ home-manager/kiara/kitty.nix
@@ -1,17 +1,7 @@
 {
-  config,
   pkgs,
   ...
-}: let
-  kitty-xterm = pkgs.writeShellScriptBin "xterm" ''
-    ${config.programs.kitty.package}/bin/kitty -1 "$@"
-  '';
-in {
-  home = {
-    packages = [kitty-xterm];
-    sessionVariables = {TERMINAL = "kitty -1";};
-  };
-
+}: {
   programs.kitty = {
     enable = true;
     package = pkgs.kitty;
