61ee71fda7871131d9ca0707ff1d80d61480062d
Kiara Grouwstra <kiara@bij1.org>
2024-03-21 01:20:24 +0100

    fix permissions of .ssh/config

--- home-manager/kiara/dotfiles.nix
+++ home-manager/kiara/dotfiles.nix
@@ -22,6 +22,12 @@ in {
     // {
       # must `git add .` or new files won't be found
       ".config/swaync/configSchema.json".source = "${pkgs.swaynotificationcenter}/etc/xdg/swaync/configSchema.json";
+      # https://github.com/nix-community/home-manager/issues/322#issuecomment-1856128020
+      ".ssh/config" = {
+        source = ./dotfiles/.ssh/config;
+        target = ".ssh/config_source";
+        onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+      };
     };
   # ".config/sops/age/keys.txt".source = config.sops.secrets.age-keys.path; # $SOPS_AGE_KEY_FILE # error: attribute 'sops' missing
 }
