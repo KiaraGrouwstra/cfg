837784203745ce28d2142b33007af0f3542426af
Kiara Grouwstra <kiara@bij1.org>
2024-03-30 00:46:26 +0100

    NIXOS_OZONE_WL = "1"; # electron

--- hosts/hammer/configuration.nix
+++ hosts/hammer/configuration.nix
@@ -116,6 +116,7 @@ in {
 
   environment.variables = {
     XDG_CONFIG_HOME = "$HOME/.config";
+    NIXOS_OZONE_WL = "1"; # electron
     QT_STYLE_OVERRIDE = "kvantum";
   };
 
@@ -138,6 +139,12 @@ in {
   services.gnome.at-spi2-core.enable = true; # orca
 
   services.gnome.gnome-keyring.enable = true; # flare-signal
+
+  # environment.sessionVariables = {
+  #   # qt wayland: https://discourse.nixos.org/t/problem-with-qt-apps-styling/29450/8
+  #   QT_QPA_PLATFORM = "wayland";
+  #   QT_QPA_PLATFORMTHEME = "qt5ct";
+  # };
 
   # let file managers access trash and remotes
   services.gvfs.enable = true;
