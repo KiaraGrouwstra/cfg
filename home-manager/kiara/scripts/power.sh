cfc83018ce22afd369b4c118b8665c6580122be9
Kiara Grouwstra <kiara@bij1.org>
2024-03-30 01:13:49 +0100

    power.sh: nix shell shebang

--- home-manager/kiara/scripts/power.sh
+++ home-manager/kiara/scripts/power.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env -S nix shell nixpkgs#gum nixpkgs#swaylock-effects niri#niri --command bash
 
 # power options
 chosen=$(printf '⏻  Power Off\n⭮  Restart\n🌙  Suspend\n❄️  Hibernate\n🚪  Log Out\n🔒  Lock' | gum choose)
