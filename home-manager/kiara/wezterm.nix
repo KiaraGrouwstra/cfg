376d33887404310aead60c9fae0cc6f6664ac8ad
Kiara Grouwstra <kiara@bij1.org>
2024-03-31 03:31:13 +0200

    wezterm: keep hands in place

--- home-manager/kiara/wezterm.nix
+++ home-manager/kiara/wezterm.nix
@@ -13,6 +13,233 @@ _: {
           set_environment_variables = {
             TERM = 'wezterm',
           },
+          keys = {
+            {
+               key = '0',
+               mods = 'ALT',
+               action = wezterm.action.ResetFontSize,
+            },
+            {
+               key = '1',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 0 },
+            },
+            {
+               key = '2',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 1 },
+            },
+            {
+               key = '3',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 2 },
+            },
+            {
+               key = '4',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 3 },
+            },
+            {
+               key = '5',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 4 },
+            },
+            {
+               key = '6',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 5 },
+            },
+            {
+               key = '7',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 6 },
+            },
+            {
+               key = '8',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = 7 },
+            },
+            {
+               key = '9',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTab = -1 },
+            },
+            {
+               key = '-',
+               mods = 'ALT',
+               action = wezterm.action.DecreaseFontSize,
+            },
+            {
+               key = '"',
+               mods = 'ALT',
+               action = wezterm.action{SplitVertical = {domain="CurrentPaneDomain"}},
+            },
+            {
+               key = '[',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTabRelative = -1 },
+            },
+            {
+               key = ']',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTabRelative = 1 },
+            },
+            {
+               key = '%',
+               mods = 'ALT',
+               action = wezterm.action{SplitHorizontal = {domain="CurrentPaneDomain"}},
+            },
+            {
+               key = '=',
+               mods = 'ALT',
+               action = wezterm.action.IncreaseFontSize,
+            },
+            {
+               key = 'c',
+               mods = 'ALT',
+               action = wezterm.action{CopyTo = "Clipboard" },
+            },
+            -- {
+            --    key = 'DownArrow',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{AdjustPaneSize = { "Down", 1 }},
+            -- },
+            {
+               key = 'DownArrow',
+               mods = 'ALT',
+               action = wezterm.action{ActivatePaneDirection = "Down" },
+            },
+            {
+               key = 'f',
+               mods = 'ALT',
+               action = wezterm.action{Search = {CaseSensitiveString=""}},
+            },
+            {
+               key = 'j',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTabRelative = 1 },
+            },
+            -- {
+            --    key = 'j',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{MoveTabRelative = 1 },
+            -- },
+            {
+               key = 'j',
+               mods = 'CTRL',
+               action = wezterm.action{ScrollByPage = 1 },
+            },
+            {
+               key = 'k',
+               mods = 'ALT',
+               action = wezterm.action{ActivateTabRelative = -1 },
+            },
+            -- {
+            --    key = 'k',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{MoveTabRelative = -1 },
+            -- },
+            {
+               key = 'k',
+               mods = 'CTRL',
+               action = wezterm.action{ScrollByPage = -1 },
+            },
+            {
+               key = 'l',
+               mods = 'ALT',
+               action = wezterm.action.ShowDebugOverlay,
+            },
+            -- {
+            --    key = 'LeftArrow',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{AdjustPaneSize = {"Left",1}},
+            -- },
+            {
+               key = 'LeftArrow',
+               mods = 'ALT',
+               action = wezterm.action{ActivatePaneDirection = "Left"},
+            },
+            {
+               key = 'm',
+               mods = 'ALT',
+               action = wezterm.action.Hide,
+            },
+            {
+               key = 'n',
+               mods = 'ALT',
+               action = wezterm.action.SpawnWindow,
+            },
+            {
+               key = 'p',
+               mods = 'ALT',
+               action = wezterm.action.ActivateCommandPalette,
+            },
+            {
+               key = 'r',
+               mods = 'ALT',
+               action = wezterm.action.ReloadConfiguration,
+            },
+            -- {
+            --    key = 'RightArrow',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{AdjustPaneSize = {"Right",1}},
+            -- },
+            {
+               key = 'RightArrow',
+               mods = 'ALT',
+               action = wezterm.action{ActivatePaneDirection = "Right"},
+            },
+            {
+               key = 'Space',
+               mods = 'ALT',
+               action = wezterm.action.QuickSelect,
+            },
+            {
+               key = 't',
+               mods = 'ALT',
+               action = wezterm.action{SpawnTab = "CurrentPaneDomain"},
+            },
+            -- {
+            --    key = 't',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{SpawnTab = "DefaultDomain"},
+            -- },
+            {
+               key = 'u',
+               mods = 'ALT',
+               action = wezterm.action.CharSelect,
+            },
+            -- {
+            --    key = 'UpArrow',
+            --    mods = 'ALT-SHIFT',
+            --    action = wezterm.action{AdjustPaneSize = {"Up",1}},
+            -- },
+            {
+               key = 'UpArrow',
+               mods = 'ALT',
+               action = wezterm.action{ActivatePaneDirection = "Up"},
+            },
+            {
+               key = 'v',
+               mods = 'ALT',
+               action = wezterm.action{PasteFrom = "Clipboard"},
+            },
+            {
+               key = 'w',
+               mods = 'ALT',
+               action = wezterm.action{CloseCurrentTab = {confirm=true}},
+            },
+            {
+               key = 'x',
+               mods = 'ALT',
+               action = wezterm.action.ActivateCopyMode,
+            },
+            {
+               key = 'z',
+               mods = 'ALT',
+               action = wezterm.action.TogglePaneZoomState,
+            },
+          },
         }
       '';
   };
