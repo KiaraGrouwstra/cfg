662694525861ca6944a942f3d26d1507b6ca3cdd
Kiara Grouwstra <kiara@noreply.codeberg.org>
2024-04-03 14:57:28 +0000

    readd command-not-found

--- hosts/hammer/configuration.nix
+++ hosts/hammer/configuration.nix
@@ -177,6 +177,8 @@ in {
     "L+ ${nixPath} - - - - ${pkgs.path}"
   ];
 
+  programs.command-not-found.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
