92f74313e038711e84f4112e9a12d1c9879d4d9a
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-04-06 12:51:56 +0200

    update guix profile

--- home-manager/kiara/zsh.zsh
+++ home-manager/kiara/zsh.zsh
@@ -4,7 +4,7 @@ set -o vi
 any-nix-shell zsh --info-right | source /dev/stdin
 
 # guix
-GUIX_PROFILE="$HOME/.guix-profile"
+GUIX_PROFILE="$HOME/.config/guix/current"
 . "$GUIX_PROFILE/etc/profile"
 
 # flatpak
@@ -13,18 +13,18 @@ export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$H
 # yazi: update current working directory on exit
 # https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
 function yy() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
 
 # markdown
 function mrkd() { pandoc ${@:1:2} | lynx -stdin; }
 # TODO: style browser for local html files
 function markdown() {
-  pandoc $1 > /tmp/$1.html
+  pandoc $1 >/tmp/$1.html
   xdg-open /tmp/$1.html
 }
