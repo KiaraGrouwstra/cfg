dc28ebd6df93057e667758c0df48833e6ad26017
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-05-03 21:03:19 +0000

    Revert "noXlibs, closes #245"
    This reverts commit 6b81daac3223cd5a1970975f9f5391e18066b28d.

--- hosts/hammer/niri.nix
+++ hosts/hammer/niri.nix
@@ -5,8 +5,6 @@
 }: {
   nixpkgs.overlays = [inputs.niri.overlays.niri];
 
-  environment.noXlibs = true;
-
   services.displayManager.defaultSession = "niri";
 
   # qt wayland: https://discourse.nixos.org/t/problem-with-qt-apps-styling/29450/8
