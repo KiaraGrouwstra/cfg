48d1267d956f83a1902dabda9c758f14d97eaee9
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-05-04 09:16:32 +0000

    rm duplicate persist for keyrings

--- home-manager/kiara/features/files/default.nix
+++ home-manager/kiara/features/files/default.nix
@@ -12,7 +12,6 @@ with config.commands; {
   home.persistence."/persist/home/kiara".directories = [
     ".config/Thunar"
     ".config/Nextcloud"
-    ".local/share/keyrings"
     ".local/share/Nextcloud"
   ];
 
