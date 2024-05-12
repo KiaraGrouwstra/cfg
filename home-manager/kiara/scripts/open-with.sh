582c06b2c91e6e8f0d67279a7c75609a0b5eb758
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-05-12 12:47:14 +0000

    update open-with.sh to properly get available apps

--- home-manager/kiara/scripts/open-with.sh
+++ home-manager/kiara/scripts/open-with.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env -S nix shell nixpkgs#xdg-utils nixpkgs#gtk3 nixpkgs#fzf --command bash
 # open the given file with its only .desktop app or by whichever the user picks
+# set -xe
 file=$1
 mime=$(xdg-mime query filetype $file)
-apps=$(cat ~/.local/share/applications/mimeinfo.cache | grep $mime | sed -E 's/.*?=//g' | sed 's/\.desktop;/\n/g' | grep -E '.+')
+apps=$(cat ~/.local/share/applications/mimeapps.list | grep -v 'open-with' | grep "${mime}=" | sed -E 's/.*?=//g' | sed 's/\.desktop;/\n/g' | sed 's/\.desktop//g' | sort | uniq | grep -E '.+')
 count=$(echo $apps | wc -w)
-if [ $count = "1" ];then
-    app=$apps
+if [ $count = "1" ]; then
+  app=$apps
 else
-    app=$(echo $apps | tr " " "\n" | fzf)
+  app=$(echo $apps | tr " " "\n" | fzf)
 fi
 # it won't pick up on gtk-launch otherwise somehow
 nix shell nixpkgs#gtk3 --command gtk-launch $app $file
