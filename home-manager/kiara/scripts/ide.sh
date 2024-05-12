eb32f05152fe318b856666d76bea8c306bc700bf
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-05-12 15:37:04 +0000

    parameterize ide.sh

--- home-manager/kiara/scripts/ide.sh
+++ home-manager/kiara/scripts/ide.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S nix shell nixpkgs#wezterm nixpkgs#lazygit nixpkgs#just nixpkgs#jaq --command bash
 # wezterm cli spawn --cwd / --new-window -- yazi --cwd-file /dev/stdout | while read -r dir; do; done
 # export dir=$(yazi --cwd-file /dev/stdout)
-export dir=$PWD
+export dir="${1:-$PWD}"
 # export pane_id=$(wezterm cli spawn --cwd $dir --new-window -- lazygit)
 export pane_id=$WEZTERM_PANE
 export window_id=$(wezterm cli list --format json | jaq "map(select(.pane_id == $pane_id)) | .[].window_id")
