5e8f2fc5331a1cec748982b5ad32fad21f33322d
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-06-23 10:48:05 +0000

    fix check-age script

--- scripts/check-age.sh
+++ scripts/check-age.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S nix shell nixpkgs#jaq nixpkgs#gum --command sh
 # Check when inputs were last updated
-cat flake.lock \
-    | jaq -r '.nodes | del(.root) | map_values(.locked.lastModified) | to_entries | sort_by(.value) | map("echo " + .key + ",\\\"$(ago.sh " + (.value | tostring) + ")\\\"")[]' \
-    | sh \
-    | gum table -c 'input,updated' -w '25,25' --height=60
+cat flake.lock |
+  jaq -r '.nodes | del(.root) | map_values(.locked.lastModified) | to_entries | sort_by(.value) | map("echo " + .key + ",\\\"$(ago " + (.value | tostring) + ")\\\"")[]' |
+  sh |
+  gum table -c 'input,updated' -w '25,25' --height=60
