9d32e01fa8b06f3e204d2e5cebfb29e1efdbb5fc
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-06-23 08:08:44 +0000

    format bash file

--- scripts/jit.sh
+++ scripts/jit.sh
@@ -4,12 +4,12 @@
 
 set -euo pipefail
 
-APPS=$(nix-locate --minimal --regex '/share/applications/.*\.desktop$' \
-	   | sed 's/\.out$//;/(.*)/d' \
-	   | sort -u)
-SELECTION=$(fzf --border --reverse --prompt='Demo Application: ' <<< "$APPS")
+APPS=$(nix-locate --minimal --regex '/share/applications/.*\.desktop$' |
+  sed 's/\.out$//;/(.*)/d' |
+  sort -u)
+SELECTION=$(fzf --border --prompt='Demo Application: ' <<<"$APPS")
 # tighten the sandbox to what you need
 # $HOME is tmpfs to the app so it puts its trash into a trash can
 # bwrap --dev-bind / / \
 #       --tmpfs "$HOME" \
-      nix run nixpkgs#"$SELECTION"
+nix run nixpkgs#"$SELECTION"
