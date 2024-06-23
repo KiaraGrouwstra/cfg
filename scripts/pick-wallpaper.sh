cfa11ddbf77d2bd686df0c0c18e0d75eaa91118a
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-06-23 11:40:10 +0000

    switch pick-wallpaper from picker gum to fzf

--- scripts/pick-wallpaper.sh
+++ scripts/pick-wallpaper.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env -S nix shell nixpkgs#gum nixpkgs#swaybg nixpkgs#wallust --command bash
+#!/usr/bin/env -S nix shell nixpkgs#fzf nixpkgs#swaybg nixpkgs#wallust --command bash
 # pick a wallpaper and set it
 export wallpaper_dir=~/Pictures/wallpapers/
-ls $wallpaper_dir | gum choose --no-limit | while read -r img ; do
+ls $wallpaper_dir | fzf --reverse --preview 'pistol {}' | while read -r img; do
   pidof swaybg && kill -9 $(pidof swaybg)
   swaybg -m fill -o '*' -i $wallpaper_dir$img
-  wallust run $wallpaper_dir$img;
+  wallust run $wallpaper_dir$img
 done
