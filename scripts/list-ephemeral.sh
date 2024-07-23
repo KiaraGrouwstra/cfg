2bc95aa9862004d9097c5cb6183e4f392d20098c
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-07-23 10:39:48 +0000

    disable problematic part of list-ephemeral.sh, workaround to #331

--- scripts/list-ephemeral.sh
+++ scripts/list-ephemeral.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env -S nix shell nixpkgs#jaq --command sh
 set -x
 DIR="${1:-$HOME}"
-find $DIR $(nix eval .#nixosConfigurations.default.config.disko.devices.disk."main".content.partitions.luks.content.content.subvolumes --json | jaq -r '.[].mountpoint | select(. != "/") | "-path " + . + " -prune -o"') $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".directories --json | jaq -r '.[].dirPath | "-path " + . + " -prune -o"') $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".files --json | jaq -r '.[].filePath | "-path " + . + " -prune -o"') $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".users.kiara.directories --json | jaq -r '.[].dirPath | "-path " + . + " -prune -o"') $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".users.kiara.files --json | jaq -r '.[].filePath | "-path " + . + " -prune -o"') $(nix eval .#nixosConfigurations.default.config.home-manager.users.kiara.home.persistence."/persist/home/kiara".directories --json | jaq -r '.[] | "-path /home/kiara/" + . + " -prune -o"') $(nix eval .#nixosConfigurations.default.config.home-manager.users.kiara.home.persistence."/persist/home/kiara".files --json | jaq -r '.[] | "-path /home/kiara/" + . + " -prune -o"') -type f -printf '%p\n'
+find "$DIR" \
+  $(nix eval .#nixosConfigurations.default.config.disko.devices.disk."main".content.partitions.luks.content.content.subvolumes --json | jaq -r '.[].mountpoint | select(. != "/") | "-path " + . + " -prune -o"') \
+  $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".directories --json | jaq -r '.[].dirPath | "-path " + . + " -prune -o"') \
+  $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".files --json | jaq -r '.[].filePath | "-path " + . + " -prune -o"') \
+  $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".users.kiara.directories --json | jaq -r '.[].dirPath | "-path " + . + " -prune -o"') \
+  $(nix eval .#nixosConfigurations.default.config.environment.persistence."/persist".users.kiara.files --json | jaq -r '.[].filePath | "-path " + . + " -prune -o"') \
+  $(nix eval .#nixosConfigurations.default.config.home-manager.users.kiara.home.persistence."/persist/home/kiara".directories --json | jaq -r '.[] | "-path /home/kiara/" + . + " -prune -o"') \
+  -type f -printf '%p\n'
+  # $(nix eval .#nixosConfigurations.default.config.home-manager.users.kiara.home.persistence."/persist/home/kiara".files --json | jaq -r '.[] | "-path /home/kiara/" + . + " -prune -o"') \
+  # ^ seems to break on persisted desktop entries containing spaces, TODO figure out how to properly escape
