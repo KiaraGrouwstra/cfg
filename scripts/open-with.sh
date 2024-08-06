d8b671b08652b953ba61a2a0e05d0b9845ac8e7b
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-08-06 17:53:53 +0000

    open-with: formatting

--- scripts/open-with.sh
+++ scripts/open-with.sh
@@ -2,13 +2,14 @@
 # open the given file with its only .desktop app or by whichever the user picks
 # set -xe
 file=$1
-mime=$(xdg-mime query filetype $file)
+mime=$(xdg-mime query filetype "$file")
+# /etc/profiles/per-user/kiara/share/applications/mimeinfo.cache
 apps=$(cat ~/.local/share/applications/mimeapps.list | grep -v 'open-with' | grep "${mime}=" | sed -E 's/.*?=//g' | sed 's/\.desktop;/\n/g' | sed 's/\.desktop//g' | sort | uniq | grep -E '.+')
-count=$(echo $apps | wc -w)
-if [ $count = "1" ]; then
+count=$(echo "$apps" | wc -w)
+if [ "$count" = "1" ]; then
   app=$apps
 else
-  app=$(echo $apps | tr " " "\n" | fzf --reverse --bind=tab:down --bind=shift-tab:up)
+  app=$(echo "$apps" | tr " " "\n" | fzf --reverse --bind=tab:down --bind=shift-tab:up)
 fi
 # it won't pick up on gtk-launch otherwise somehow
-nix shell nixpkgs#gtk3 --command gtk-launch $app $file
+nix shell nixpkgs#gtk3 --command gtk-launch "$app" "$file"
