697aaba9a50940134ffcf053a5e228403222bb7c
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-08-31 14:06:28 +0000

    ollama: rm deprecated sandbox option

--- hosts/hammer/ollama.nix
+++ hosts/hammer/ollama.nix
@@ -4,7 +4,6 @@
     host = "127.0.0.1";
     port = 11434;
     acceleration = "rocm";
-    sandbox = false;
   };
   # nginx by default runs with ProtectHome = true
   environment.persistence."/persist" = {
