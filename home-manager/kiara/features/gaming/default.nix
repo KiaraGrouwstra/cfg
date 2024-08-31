ec9a925afdb0d9220b8fd1a2785a5356fe1e1f5c
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-08-31 09:06:07 +0000

    persist steam

--- home-manager/kiara/features/gaming/default.nix
+++ home-manager/kiara/features/gaming/default.nix
@@ -5,13 +5,16 @@
 }: {
   home.persistence."/persist/home/kiara".directories = [
     ".local/share/applications/wine"
-    ".local/share/Steam"
     ".local/share/lutris"
     ".local/share/citra-emu"
-    ".steam"
     "Desktop"
     ".config/antimicrox"
     ".config/citra-emu"
+    # steam
+    ".local/share/flatpak"
+    ".var"
+    ".steam"
+    ".local/share/Steam"
   ];
   home.persistence."/persist/home/kiara".files = [
     ".local/share/applications/wine-extension-chm.desktop"
