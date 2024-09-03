2e262b4462e87344c4a6ecfcb2481997ebb74db6
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-09-03 06:25:01 +0000

    rm chromebook

--- home-manager/kiara/chromebook.nix
+++
@@ -1,9 +0,0 @@
-# https://wiki.nixos.org/wiki/Installing_Nix_on_Crostini
-{pkgs, inputs, ...}: {
-  # register desktop applications
-  xdg.configFile."systemd/user/cros-garcon.service.d/override.conf".text = ''
-    [Service]
-    Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
-    Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:%h/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
-  '';
-}
