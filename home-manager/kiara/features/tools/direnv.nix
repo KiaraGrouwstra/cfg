c6456b6fb4fc8167caa7c6190ea9b299825b31f3
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-09-23 20:47:44 +0200

    direnv: less chatty

--- home-manager/kiara/features/tools/direnv.nix
+++ home-manager/kiara/features/tools/direnv.nix
@@ -3,13 +3,6 @@
     ".local/share/direnv"
   ];
 
-  home.sessionVariables = {
-    # No more long command warnings
-    DIRENV_WARN_TIMEOUT = "24h";
-    # No more usesless logs
-    DIRENV_LOG_FORMAT = "";
-  };
-
   programs = {
     direnv = {
       enable = true;
